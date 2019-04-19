a3e2594bd1d880233b154e658ee3b40f6b32fdf2
Chentao Tang <chentao.tang@mail.utoronto.ca>
2019-04-18 21:30:44 -0400

    completed all api routes and survey functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,21 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
+        var scores = req.body.scores
+        var compatVal = 100;
+        var partner = {};
+        for (let i = 0; i < friends.length; i++) {
+            var compatSum = 0;
+            for (let j = 0; j < friends[i].scores.length; j++) {
+                compatSum += Math.abs(friends[i].scores[j]-scores[j]);
+            }
+            if (compatSum < compatVal) {
+                compatVal = compatSum;
+                partner = friends[i];    
+            }
+        }
+
+        res.json(partner);
     });
 
 
