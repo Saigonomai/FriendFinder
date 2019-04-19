c1d5e7dbcbedf28f595f1c7c59c0c4a8d425949f
Chentao Tang <chentao.tang@mail.utoronto.ca>
2019-04-18 20:06:36 -0400

    Populated data, completed html,added html routes, and added one api route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
+    var friends = require('../data/friends.js');
 
     app.get("/api/friends", function(req, res) {
+        return res.json(friends);
     });
 
     app.post("/api/friends", function(req, res) {
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 var path = require("path");
 
 app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
 app.get("*", function(req, res) {
